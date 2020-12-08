5022b01ce74e42cd6e6492ac59fefc607950f9c3
zhenght <284989165@qq.com>
2020-12-08 21:25:45 +0800

    modify test

--- test/test_timer/main.cpp
+++ test/test_timer/main.cpp
@@ -4,28 +4,49 @@
 
 class Timeout : public std::enable_shared_from_this<Timeout> {
   public:
-    Timeout(pump::service *sv) { sv_ = sv; }
+    Timeout(pump::service *sv) {
+        sv_ = sv;
+
+        count_ = 0;
+        last_report_time_ = 0;
+    }
 
     void start() {
         printf("new timeout\n");
-        pump::time::timer_callback cb =
-            pump_bind(&Timeout::on_timer_timeout, this);
-        t_ = pump::time::timer::create(1000, cb, true);
-        if (!sv_->start_timer(t_)) {
-            printf("start timeout error\n");
+        pump::time::timer_callback cb = pump_bind(&Timeout::on_timer_timeout, this);
+        for (int i = 0; i < 20000; i++) {
+            auto t = pump::time::timer::create(10, cb, true);
+            if (!sv_->start_timer(t)) {
+                printf("start timeout error\n");
+            }
+            timers_.push_back(t);
         }
+
     }
 
     /*********************************************************************************
      * Timer timeout callback
      ********************************************************************************/
     void on_timer_timeout() {
-        printf("timeout event pending %d\n", (int32_t)time(0));
+
+        count_++;
+
+        int32_t now = (int32_t)::time(0);
+        if (last_report_time_ != now) {
+            printf("timer pending count %d\n", count_);
+            last_report_time_ = now;
+            count_ = 0;
+        }
     }
 
   private:
     pump::service *sv_;
-    std::shared_ptr<pump::time::timer> t_;
+
+    typedef std::shared_ptr<pump::time::timer> timer_sptr;
+    std::vector<timer_sptr> timers_;
+
+    int32_t count_;
+    int32_t last_report_time_;
 };
 
 int main(int argc, const char **argv) {
