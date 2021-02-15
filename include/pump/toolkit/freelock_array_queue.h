4e1b263fe3eff5b921eac0c3bd2dfb6befd21f3c
zhenght <284989165@qq.com>
2021-02-16 07:37:48 +0800

    fix freelock array queue codes

--- include/pump/toolkit/freelock_array_queue.h
+++ include/pump/toolkit/freelock_array_queue.h
@@ -40,112 +40,130 @@ namespace toolkit {
         // Element type size
         const static uint32_t element_size = sizeof(element_type);
 
+        struct element_node {
+            volatile int32_t ready;
+            block_t data[element_size];
+        };
+
       public:
         /*********************************************************************************
          * Constructor
          ********************************************************************************/
         freelock_array_queue(uint32_t size)
             : size_(0),
-              mem_block_(nullptr),
+              size_mask_(0),
+              nodes_(nullptr),
               write_index_(0),
-              max_write_index_(0),
-              read_index_(0),
-              max_read_index_(0) {
-            // Init element size.
+              read_index_(0) {
+            // Init array size.
             size_ = ceil_to_pow2(size);
-            // Init element size mask.
-            element_size_mask_ = size_ - 1;
-            // Create memory block.
-            mem_block_ = (block_t*)pump_malloc(size * element_size);
+            // Init array size mask.
+            size_mask_ = size_ - 1;
+            // Create element nodes.
+            nodes_ = (element_node*)pump_malloc(size * sizeof(element_node));
+            memset(nodes_, 0, size * sizeof(element_node));
         }
 
         /*********************************************************************************
          * Deconstructor
          ********************************************************************************/
         ~freelock_array_queue() {
-            if (mem_block_) {
-                int32_t read_index = read_index_.load();
-                int32_t max_read_index = max_read_index_.load();
-                while (__count_to_index(read_index) != __count_to_index(max_read_index)) {
-                    ((element_type *)mem_block_ + __count_to_index(read_index++))
-                        ->~element_type();
+            if (nodes_) {
+                int32_t beg = read_index_.load();
+                int32_t end = write_index_.load();
+                for (int32_t i = beg; i < end; i++) {
+                    ((element_type*)nodes_[__count_to_index(i)].data)->~element_type();
                 }
-                pump_free(mem_block_);
+                pump_free(nodes_);
             }
         }
 
         /*********************************************************************************
          * Push
-         * Return false if array is full, thread safe.
          ********************************************************************************/
-        bool push(const element_type &data) {
+        template <typename U>
+        bool push(U &&data) {
+            // Get current write index.
             int32_t cur_write_index = write_index_.load(std::memory_order_acquire);
-            do {
-                // Array is full
-                if (__count_to_index(cur_write_index + 1) ==
-                    __count_to_index(max_write_index_.load(std::memory_order_acquire))) {
-                    return false;
+            // Get max write index.
+            int32_t max_write_index = read_index_.load(std::memory_order_relaxed);
+            // Dest Element node.
+            element_node *elem_node = nodes_ + __count_to_index(cur_write_index);
+
+          try_again:
+            // Check the node is ready status.
+            while (elem_node->ready == 1) {
+                // If the queue full just return.
+                if (cur_write_index >= max_write_index) {
+                    max_write_index = read_index_.load(std::memory_order_acquire);
+                    if (cur_write_index >= max_write_index) {
+                        return false;
+                    }
                 }
-
-                if (write_index_.compare_exchange_strong(cur_write_index,
-                                                         cur_write_index + 1,
-                                                         std::memory_order_acquire,
-                                                         std::memory_order_relaxed)) {
-                    break;
-                }
-            } while (true);
-
-            // Construct element object
-            new ((element_type *)mem_block_ + __count_to_index(cur_write_index)) element_type(data);
-
-            int32_t index = cur_write_index;
-            while (!max_read_index_.compare_exchange_weak(index,
-                                                          index + 1,
-                                                          std::memory_order_release,
-                                                          std::memory_order_relaxed)) {
-                index = cur_write_index;
+                cur_write_index = write_index_.load(std::memory_order_relaxed);
+                elem_node = nodes_ + __count_to_index(cur_write_index);
             }
+
+            if (!write_index_.compare_exchange_strong(cur_write_index,
+                                                      cur_write_index + 1,
+                                                      std::memory_order_acquire,
+                                                      std::memory_order_relaxed)) {
+                elem_node = nodes_ + __count_to_index(cur_write_index);
+                goto try_again;
+            }
+
+            // Construct element data.
+            new ((element_type*)elem_node->data) element_type(std::forward<U>(data));
+
+            // Mark element node ready.
+            elem_node->ready = 1;
 
             return true;
         }
 
         /*********************************************************************************
          * Pop
-         * Return false if array is empty, thread safe.
          ********************************************************************************/
         template <typename U>
         bool pop(U &data) {
-            int32_t cur_read_index = read_index_.load(std::memory_order_relaxed);
-            do {
-                int32_t array_read_index = __count_to_index(cur_read_index);
-                if (array_read_index == __count_to_index(max_read_index_.load(std::memory_order_acquire))) {
-                    return false;
-                }
+            // Get current read index.
+            int32_t cur_read_index = read_index_.load(std::memory_order_acquire);
+            int32_t max_read_index = write_index_.load(std::memory_order_relaxed);
+            // Dest element node
+            element_node *elem_node = nodes_ + __count_to_index(cur_read_index);
+            // Dest element
+            element_type *elem = nullptr;
 
-                if (read_index_.compare_exchange_strong(cur_read_index,
-                                                        cur_read_index + 1,
-                                                        std::memory_order_acquire,
-                                                        std::memory_order_relaxed)) {
-                    // Copy element.
-                    element_type *elem = (element_type*)mem_block_ + array_read_index;
-                    data = std::move(*elem);
-                    // Deconstruct element.
-                    elem->~element_type();
-
-                    int32_t index = cur_read_index;
-                    while (!max_write_index_.compare_exchange_weak(index,
-                                                                   index + 1,
-                                                                   std::memory_order_release,
-                                                                   std::memory_order_relaxed)) {
-                        index = cur_read_index;
+          try_again:
+            // Check the node is ready status.
+            while (elem_node->ready == 0) {
+                if (cur_read_index == max_read_index) {
+                    max_read_index = write_index_.load(std::memory_order_acquire);
+                    if (cur_read_index == max_read_index) {
+                        return false;
                     }
-
-                    return true;
                 }
-            } while (true);  // keep looping to try again!
+                cur_read_index = read_index_.load(std::memory_order_relaxed);
+                elem_node = nodes_ + __count_to_index(cur_read_index);
+            }
 
-            // Add this return statement to avoid compiler warnings
-            return false;
+            if (!read_index_.compare_exchange_strong(cur_read_index,
+                                                     cur_read_index + 1,
+                                                     std::memory_order_acquire,
+                                                     std::memory_order_relaxed)) {
+                elem_node = nodes_ + __count_to_index(cur_read_index);
+                goto try_again;
+            }
+
+            // Pop element.
+            elem = (element_type*)(elem_node->data);
+            data = std::move(*elem);
+            elem->~element_type();
+
+            // Mark element not ready.
+            elem_node->ready = 0;
+
+            return true;
         }
 
         /*********************************************************************************
@@ -181,30 +199,22 @@ namespace toolkit {
          * Count to index
          ********************************************************************************/
         PUMP_INLINE int32_t __count_to_index(int32_t count) const {
-            return (count & element_size_mask_);
+            return (count & size_mask_);
         }
 
       private:
-        // Capacity size
+        // Size
         int32_t size_;
+        // Size mask
+        int32_t size_mask_;
 
-        // Element memory block
-        block_t *mem_block_;
-
-        // Element size mask
-        int32_t element_size_mask_;
+        // Element nodes
+        element_node *nodes_;
 
         // Next write index
         std::atomic_int32_t write_index_;
-        // Max write index
-        // It should be equal or littel read index at all
-        std::atomic_int32_t max_write_index_;
-
         // Next read index
         std::atomic_int32_t read_index_;
-        // Max read index
-        // It should be equal write index at all
-        std::atomic_int32_t max_read_index_;
     };
 
 }  // namespace toolkit
